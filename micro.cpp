3a507aeefdaf7d22f43e3ab65b024b53ac1eab1d
Togekey <zzyzzy@outlook.com>
2018-03-11 18:23:45 +0800

    editor in n2 for branch graph

--- micro.cpp
+++ micro.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main()
 {
 	cout << "hello, micro" << endl;
-	cout << "edited in branch n1" << endl;
+	cout << "edited in branch n2 and n1 has been deleted." << endl;
 	return 0;
 }
