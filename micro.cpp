5d8da02b6ad83dbeb7d7409cf9a9d59c16aebc97
Togekey <zzyzzy@outlook.com>
2018-03-11 18:38:44 +0800

    class test

--- micro.cpp
+++ micro.cpp
@@ -2,9 +2,35 @@
 
 using namespace std;
 
+class test
+{
+	public:
+	int a;
+	int b;
+	test()
+	{
+		a = 0;
+		b = 0;
+	}
+	test(int a_, int b_)
+	{
+		a = a_;
+		b = b_;
+	}
+	int print()
+	{
+		cout << a << b << endl;
+		return a;
+	}
+};
+
 int main()
 {
+	test zero;
+	int c = zero.print();
+	test a(3,9);
+	int b = a.print();
 	cout << "hello, micro" << endl;
-	cout << "edited in branch n1" << endl;
+	cout << b << endl;
 	return 0;
 }
